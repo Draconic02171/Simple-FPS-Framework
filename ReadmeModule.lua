edbe126187229bc1f7ce3fc6483f429cfdd0bf51
Draconic02171 <draconic998877@gmail.com>
2025-05-26 17:33:56 +0700

    Update some mistakes

---
+++ ReadmeModule.lua
@@ -0,0 +1,42 @@
+--[[
+==========================================================================
+███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗    ███████╗██████╗ ███████╗
+██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝    ██╔════╝██╔══██╗██╔════╝
+███████╗██║██╔████╔██║██████╔╝██║     █████╗      █████╗  ██████╔╝███████╗
+╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝      ██╔══╝  ██╔═══╝ ╚════██║
+███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗    ██║     ██║     ███████║
+╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝    ╚═╝     ╚═╝     ╚══════╝
+
+---=== Simple FPS Framework ===---
+--------- by Draconic02171 ---------
+
+Thank you for checking out the **Simple FPS Framework**!
+==========================================================================
+
+This project is for everyone who wants a simple shooting system for your map.
+
+📂 **Features**  
+- Modular and organized architecture
+- Easy-to-customize components  
+- Beginner-friendly code structure  
+
+## 📜 License
+
+This project is open-source under the **MIT License**.
+
+You are free to use, modify, and distribute this framework — even for commercial projects — provided that you include the original copyright.
+
+* 📄 [MIT License File](LICENSE)
+* 📘 [Read Full MIT License](https://opensource.org/licenses/MIT)
+
+---
+
+📱 **Connect With Me**
+
+Stay updated or get in touch through my socials:
+
+- YouTube: [https://www.youtube.com/@draconic_lilly](https://www.youtube.com/@draconic_lilly)  
+- Discord: `draconic_nate`
+
+Happy developing!
+]]--
